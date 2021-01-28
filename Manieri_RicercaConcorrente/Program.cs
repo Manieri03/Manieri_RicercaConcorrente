df9cec6213931f803e1fbc2d67eb0c1ff2fcd8a1
Alessio Manieri <mani.alexander03@gmail.com>
2021-01-28 11:10:48 +0100

    Aggiungere i file di progetto.

---
+++ Manieri_RicercaConcorrente/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Manieri_RicercaConcorrente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[100];
+        }
+
+        static void riempiArray(int[] array)
+        {
+            var random = new Random();
+
+            for (int i = 0; i <= 99; i++)
+                array[i]=(random.Next(100));
+            
+        }
+    }
+}
